ca871e811ca389be7850fe756ae3f3c51767305f
richardbang83 <richard.bang83@gmail.com>
2023-11-16 03:34:53 +0900

    Reactive Extension Study 프로젝트 추가

---
+++ RxUIStudyApp/ViewModelLocator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Splat;
+
+namespace RxUIStudyApp
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            // Locator.CurrentMutable.register
+        }
+    }
+}
