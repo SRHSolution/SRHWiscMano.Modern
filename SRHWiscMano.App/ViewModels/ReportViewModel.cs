704efb56034d1c02dd5e5ee0bc5094bcdafee467
richardbang83 <richard.bang83@gmail.com>
2024-05-18 02:50:05 +0900

    Result View 디자인코드 작성 PlotModel 추가 수정

--- SRHWiscMano.App/ViewModels/ReportViewModel.cs
+++ SRHWiscMano.App/ViewModels/ReportViewModel.cs
@@ -19,15 +19,15 @@ namespace SRHWiscMano.App.ViewModels
         private readonly ILogger<ReportViewModel> logger;
         private readonly SharedService sharedService;
 
-        [ObservableProperty] private PlotModel pressureModel;
-        [ObservableProperty] private PlotController pressureModelController;
+        [ObservableProperty] private PlotModel modelPressureMax;
+        [ObservableProperty] private PlotController cntrPressureMax;
 
-        [ObservableProperty] private PlotModel pressureModelAtVP;
-        [ObservableProperty] private PlotController pressureModelAtVPController;
-        [ObservableProperty] private PlotModel pressureModelAtTB;
-        [ObservableProperty] private PlotController pressureModelAtTBController;
-        [ObservableProperty] private PlotModel pressureGradientModel;
-        [ObservableProperty] private PlotController pressureGradientController;
+        [ObservableProperty] private PlotModel modelPressureMaxAtVP;
+        [ObservableProperty] private PlotController cntrPressureMaxAtVP;
+        [ObservableProperty] private PlotModel modelPressureMaxAtTB;
+        [ObservableProperty] private PlotController cntrPressureMaxAtTB;
+        [ObservableProperty] private PlotModel modelPressureGradient;
+        [ObservableProperty] private PlotController cntrPressureGradient;
 
         /// <summary>
         /// Designer Datacontext를 위한 생성자
@@ -40,12 +40,13 @@ namespace SRHWiscMano.App.ViewModels
         {
             this.logger = logger;
             this.sharedService = sharedService;
-            PressureModel = CreatePressurePlot("Pressure Maximum");
-            PressureModelAtVP = CreatePressurePlot("Pressure Maximum at VP");
-            PressureModelAtTB = CreatePressurePlot("Pressure Maximum at TB");
+            ModelPressureMax = CreatePlotForPressureMax("Pressure Maximum");
+            ModelPressureMaxAtVP = CreatePlotForPressureMax("Pressure Maximum at VP");
+            ModelPressureMaxAtTB = CreatePlotForPressureMax("Pressure Maximum at TB");
+            ModelPressureGradient = CreatePlotForPressureGradient("Pressure Gradient");
         }
 
-        private PlotModel CreatePressurePlot(string title)
+        private PlotModel CreatePlotForPressureMax(string title)
         {
             var plotModel = new PlotModel();
             plotModel.Title = title;
@@ -54,12 +55,12 @@ namespace SRHWiscMano.App.ViewModels
 
             });
 
-            AddAxes(plotModel);
+            AddAxesForPressureMax(plotModel);
             plotModel.InvalidatePlot(true);
             return plotModel;
         }
 
-        private void AddAxes(PlotModel model)
+        private void AddAxesForPressureMax(PlotModel model)
         {
             foreach (var axis in model.Axes)
             {
@@ -133,5 +134,61 @@ namespace SRHWiscMano.App.ViewModels
 
         }
 
+        private PlotModel CreatePlotForPressureGradient(string title)
+        {
+            var plotModel = new PlotModel();
+            plotModel.Title = title;
+            plotModel.Series.Add(new LineSeries()
+            {
+
+            });
+
+            AddAxesForPressureGradient(plotModel);
+            plotModel.InvalidatePlot(true);
+            return plotModel;
+        }
+
+        private void AddAxesForPressureGradient(PlotModel model)
+        {
+            foreach (var axis in model.Axes)
+            {
+                model.Axes.Remove(axis);
+            }
+
+            // Y-Axis
+            model.Axes.Add(new LinearAxis()
+            {
+                IsPanEnabled = false,
+                IsZoomEnabled = false,
+                Position = AxisPosition.Left,
+                MaximumPadding = 0,
+                MinimumPadding = 0,
+
+                // Minimum = 0, // 초기 시작값
+                // Maximum = ((ySize + 1)), // 초기 최대값
+                // AbsoluteMinimum = -0, // Panning 최소값
+                // AbsoluteMaximum = ((ySize + 1)), // Panning 최대값, LineSeries는 데이터를 한 step shift 하여 표시하기 위해 ySize+1 한다
+                IsAxisVisible = true,
+                Tag = "Y"
+            });
+
+            // X-Axis
+            var xAxis = new CategoryAxis()
+            {
+                IsZoomEnabled = false,
+                IsPanEnabled = false,
+                // LabelFormatter = value => $"{value / 100}",
+                Position = AxisPosition.Bottom,
+                MinimumPadding = 0,
+                // Minimum = 0,
+                // Maximum = xSize - 1,
+                // MajorStep = xSize,
+                // MinorStep = xSize, // 최대 범위를 입력하여 MinorStep 이 표시되지 않도록 한다
+                // AbsoluteMinimum = 0,
+                // AbsoluteMaximum = xSize - 1,
+                IsAxisVisible = true,
+                Tag = "X"
+            };
+        }
     }
 }
